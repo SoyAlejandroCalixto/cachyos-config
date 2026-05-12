f01aa78f976e2e56a2cf121a1886365344cdd0ad
soyalejandrocalixto <soyalejandrocalixto@gmail.com>
2026-05-12 09:09:44 +0100

    Adaptado a la versión actual

--- install.sh
+++ install.sh
@@ -1,42 +1,45 @@
-sudo -v # make sudo never ask me for a password
+sudo -v # Prevent sudo from asking for your password again
 while true; do sudo -n true; sleep 60; done 2>/dev/null &
 SUDO_PID=$!
 
-# Install extra packages
-sudo pacman -S --needed --noconfirm fzf bitwarden kvantum pamac-aur rocm-opencl-runtime
-paru -S --noconfirm --needed fnm cloudflare-warp-bin antigravity
+# -- Package installation --
+sudo pacman -Rns --noconfirm vim kitty wofi grim # Remove unused
 
-# Clone my Hyprland config
+sudo pacman -S --noconfirm --needed git github-cli neovim hyprland hyprpaper lua lua54 lua54-lgi playerctl socat zsh noto-fonts-emoji adobe-source-han-sans-jp-fonts ttf-cascadia-code-nerd mpv eog polkit-kde-agent xdg-desktop-portal-hyprland xdg-desktop-portal-gtk gnome-themes-extra fastfetch wl-clipboard wtype yazi dolphin zoxide atuin zsh-autosuggestions zsh-syntax-highlighting ghostty discord dunst fontconfig lsd bat fzf bitwarden
+paru -S --noconfirm --needed brave-bin eww rofi-wayland rofimoji cliphist hyprshot pear-desktop fnm
+
+mkdir -p ~/.local/share/fonts # Install fonts that do not exist as a package
+git clone https://github.com/simpals/onest.git /tmp/onest
+mv /tmp/onest/fonts/ttf/*.ttf "$HOME/.local/share/fonts/"
+rm -rf /tmp/onest
+
+# -- Cloning dotfiles --
 git clone https://github.com/SoyAlejandroCalixto/arch4devs $HOME/arch4devs
-cd $HOME/arch4devs
-./install.sh
-sudo rm -rf $HOME/arch4devs && sudo rm -rf $HOME/.git && sudo rm -rf $HOME/README.md && sudo rm -rf $HOME/LICENSE && sudo rm -rf $HOME/.gitignore # Clean repo trash
+cp -r ~/arch4devs/. ~/
+sudo rm -rf ~/.git && sudo rm -rf ~/README.md && sudo rm -rf ~/install.sh && sudo rm -rf ~/LICENSE && sudo rm -rf ~/.gitignore # Clean repo trash
 
-# Remove unused packages
-sudo pacman -Rns --noconfirm vim kitty alacritty eog
-
-# Add fnm to path
-echo -e "\neval \"\$(fnm env --use-on-cd --shell zsh)\"" >> $HOME/.zshrc
-
-# Hyprland monitors settings
-cat << EOF > $HOME/.config/hypr/monitors.conf
+# -- Files --
+cat << EOF > $HOME/.config/hypr/monitors.conf # Monitors settings
 monitor=HDMI-A-1,1920x1080@75,0x0,1
 monitor=DP-2,1920x1080@60,1920x0,1
 EOF
 
-# Ranger config and plugins
-mkdir -p $HOME/.config/ranger/plugins
-git clone https://github.com/alexanderjeurissen/ranger_devicons $HOME/.config/ranger/plugins/ranger_devicons
-echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
-git clone https://github.com/maximtrp/ranger-archives.git $HOME/.config/ranger/plugins/ranger-archives
-echo "set preview_images true" >> $HOME/.config/ranger/rc.conf
-echo "set preview_images_method iterm2" >> $HOME/.config/ranger/rc.conf
+cat << EOF > $HOME/.gitconfig # Git config
+[credential "https://github.com"]
+	helper =
+	helper = !/usr/bin/gh auth git-credential
+[credential "https://gist.github.com"]
+	helper =
+	helper = !/usr/bin/gh auth git-credential
+[user]
+	email = soyalejandrocalixto@gmail.com
+	name = soyalejandrocalixto
+[core]
+	editor = antigravity --wait
+	autocrlf = input
+EOF
 
-# Cloudflare Warp config
-sudo systemctl enable warp-svc
-sudo systemctl start warp-svc
-warp-cli registration new
-
-echo -e "\e[32mFinished.\e[0m\n"
+# -- Others tweaks --
+chsh -s /bin/zsh
 
 trap "kill $SUDO_PID 2>/dev/null" EXIT # kill the process that keeps sudo without password
